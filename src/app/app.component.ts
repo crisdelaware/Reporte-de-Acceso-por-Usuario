ac8bd580b88426c746627575fd89d09ae211384e
crisdelaware <crisdelaware@gmail.com>
2023-04-11 14:56:40 -0600

    Funcionalidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ReporteAccesoUsuario';
+  miFormulario: FormGroup;
+
+  constructor() {
+    this.miFormulario = new FormGroup({
+      clave: new FormControl('', Validators.required),
+      start: new FormControl('', Validators.required),
+      end: new FormControl('', Validators.required)
+    })
+  }
+
+
+  get clave() {
+    return this.miFormulario.get('clave');
+  }
+
+  get inicio() {
+    return this.miFormulario.get('inicio');
+  }
+
+  get fin() {
+    return this.miFormulario.get('fin');
+  }
+
+  filtrar() {
+  
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
